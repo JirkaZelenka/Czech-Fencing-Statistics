889c7df2e63ab350c0d91417e5d9c7d928228443
Jirka Zelenka <jirka.zelenka@merchantee.io>
2024-04-08 02:12:39 +0200

    test deploy change

--- apps/app.py
+++ apps/app.py
@@ -1,49 +1,28 @@
-
-# ! This one is functional minimal plotly on my fencing data
-
-# Import packages
-from dash import Dash, html, dash_table, dcc, callback_context
+from dash import Dash, html, dcc
 import pandas as pd
 import sqlite3
 import plotly.express as px
 
 app = Dash(__name__)
-
 server = app.server
 
 source_file = f'./fencing_all.db'
-
 conn = sqlite3.connect(source_file)
 query = "SELECT * FROM fencers"
 df = pd.read_sql_query(query, conn)
 conn.close()
 
-"""
-df2 = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-"""
-
-# takto olabeluju jen ty existující. a pořád mi to stejně dělá dvojbiny.../
-#bins = sorted(df['birthyear'].unique())
-#labels = [str(binn) for binn in bins]  
-
 bins = list(range(df['birthyear'].min(), df['birthyear'].max()+1))
 labels = [str(binn) for binn in bins]  
 
-fig = px.histogram(df, x='birthyear', color='sex', title='Histogram of Records Count by Year and Sex', nbins=len(bins))
+fig = px.histogram(df, x='birthyear', color='sex', title='TEST Histogram of Records Count by Year and Sex', nbins=len(bins))
 fig.update_xaxes(tickvals=bins, ticktext=labels)
 
 app.layout = html.Div([
     html.Div(children='My First App with Data'),
-    #dash_table.DataTable(data=df.to_dict('records'), page_size=20),
     dcc.Graph(figure=px.histogram(df, x='birthyear',  color='club_id', nbins=len(df['birthyear'].unique()))),
-    dcc.Graph(figure=fig),
-    """
-    html.Div(children='My Second App with Data'),
-    dcc.Graph(figure=px.histogram(df2, x='continent', y='lifeExp', histfunc='avg')),
-    dash_table.DataTable(data=df2.to_dict('records'), page_size=20),
-    """
+    dcc.Graph(figure=fig)
 ])
 
-# Run the app
 if __name__ == '__main__':
     app.run(debug=True, port = 8051)
