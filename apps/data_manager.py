f3db1e1529d47049cccdc64d540c1eee70ca7f55
Jirka Zelenka <jirka.zelenka@merchantee.io>
2024-11-08 23:37:01 +0100

    new structure of the database

--- apps/data_manager.py
+++ apps/data_manager.py
@@ -14,12 +14,53 @@ class DataManager:
     def _get_connection(self):
         
         try:
-            conn = sqlite3.connect(f"{self.cf.project_path}/{self.cf.db_name}")
+            conn = sqlite3.connect(f"{self.cf.project_path}/{self.cf.db_name}_2")
             return conn
         except sqlite3.Error as e:
             print(f'Error connecting to database: {e}') 
 
             return None
+        
+    def clear_table(self, table_name):
+        
+        with self._get_connection() as conn: 
+            try:
+                cursor = conn.cursor()
+                cursor.execute(f"DELETE FROM {table_name}")
+                conn.commit()
+                cursor.close()
+            except sqlite3.Error as e:
+                print(f'Error clearing table {table_name}: {e}') 
+                conn.rollback()
+                    
+                    
+    def drop_table(self, table_name):
+        
+        with self._get_connection() as conn: 
+            try:
+                cursor = conn.cursor()
+                cursor.execute(f"DROP table {table_name}")
+                conn.commit()
+                cursor.close()
+            except sqlite3.Error as e:
+                print(f'Error dropping table {table_name}: {e}') 
+                conn.rollback()
+                       
+                       
+    def create_table(self, table_name):
+        
+        with self._get_connection() as conn: 
+        
+            table = self.cf.table_definitions[table_name]
+            try:
+                cursor = conn.cursor()
+                cursor.execute(table)
+                conn.commit()
+                cursor.close()
+            except sqlite3.Error as e:
+                print(f'Error creating table {table_name}: {e}') 
+                conn.rollback()
+                         
 
     def get_all_data(self, table_name="tournament_results"):
         
--- config.py
+++ config.py
@@ -8,3 +8,98 @@ class Config:
         
         self.project_path = os.getenv("PROJECT_PATH") 
         self.db_name = os.getenv("DB_NAME") 
+
+
+        self.table_definitions={
+            "clubs": 
+                    """ 
+                    CREATE TABLE IF NOT EXISTS clubs (
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL,
+                    city VARCHAR(255) NOT NULL,
+                    country VARCHAR(255) NOT NULL,
+                    
+                    created_at datetime NOT NULL,
+                    updated_at datetime NOT NULL,
+                    deleted_at datetime NOT NULL
+                    ); 
+                    """,
+            "tournaments": 
+                    """ 
+                    CREATE TABLE IF NOT EXISTS tournaments (
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL,
+                    city VARCHAR(255) NOT NULL,
+                    type VARCHAR(255) NOT NULL,
+                    
+                    created_at datetime NOT NULL,
+                    updated_at datetime NOT NULL,
+                    deleted_at datetime NOT NULL
+                    ); 
+                    """,
+            "fencers": 
+                    """ 
+                    CREATE TABLE IF NOT EXISTS fencers (
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL,
+                    club_id REFERENCES clubs (id),
+                    country VARCHAR(255) NOT NULL,
+                    gender VARCHAR(255) NOT NULL,
+                    birthyear INTEGER NOT NULL,
+                    
+                    created_at datetime NOT NULL,
+                    updated_at datetime NOT NULL,
+                    deleted_at datetime NOT NULL
+                    ); 
+                    """,
+            "tournament_results": 
+                    """ 
+                    CREATE TABLE IF NOT EXISTS tournament_results (
+                    id SERIAL PRIMARY KEY,
+                    event_id REFERENCES events (id),
+                    fencer_id REFERENCES fencers (id),
+                    finished VARCHAR(255) NOT NULL,
+                    first_rank INTEGER NOT NULL,
+                    final_rank INTEGER NOT NULL,
+                    percentile FLOAT NOT NULL,
+                    victories INTEGER NOT NULL,
+                    victories_perc FLOAT NOT NULL,
+                    ts INTEGER NOT NULL,
+                    tr INTEGER NOT NULL,
+                    
+                    created_at datetime NOT NULL,
+                    updated_at datetime NOT NULL,
+                    deleted_at datetime NOT NULL
+                    ); 
+                    """,
+            "events": 
+                    """ 
+                    CREATE TABLE IF NOT EXISTS events (
+                    id SERIAL PRIMARY KEY,
+                    tournament_id REFERENCES tournaments (id),
+                    date VARCHAR(255) NOT NULL,
+                    gender VARCHAR(255) NOT NULL,
+                    finished VARCHAR(255) NOT NULL,
+                    
+                    created_at datetime NOT NULL,
+                    updated_at datetime NOT NULL,
+                    deleted_at datetime NOT NULL
+                    ); 
+                    """,   
+            "duels": 
+                    """ 
+                    CREATE TABLE IF NOT EXISTS duels (
+                    id SERIAL PRIMARY KEY,
+                    event_id REFERENCES events (id),
+                    type VARCHAR(255) NOT NULL,
+                    winner_id REFERENCES fencers (id),
+                    loser_id REFERENCES fencers (id),
+                    winner_score INTEGER NOT NULL,
+                    loser_score INTEGER NOT NULL,
+                    
+                    created_at datetime NOT NULL,
+                    updated_at datetime NOT NULL,
+                    deleted_at datetime NOT NULL
+                    ); 
+                    """                       
+        }
